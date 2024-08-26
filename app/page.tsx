5061f8a1db59929a19c75e97c413baaf044a385a
chill <chill@mail.com>
2024-08-26 12:25:36 +0300

    Add Chat Form template

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import ChatForm from "@/components/ChatForm";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-5"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-5">
+      <ChatForm />
+    </main>
   );
 }
---
+++ components/ChatForm.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ChatForm() {
+  return <div>ChatForm</div>;
+}
