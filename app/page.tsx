9e1ec914a3e2e204e933d9ad9105f678417fb450
chill <chill@mail.com>
2024-08-26 12:44:54 +0300

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ChatForm from "@/components/ChatForm";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-5">
+    <main className="flex min-h-screen items-center justify-center p-5">
       <ChatForm />
     </main>
   );
--- components/ChatForm.tsx
+++ components/ChatForm.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function ChatForm() {
-  return <div>ChatForm</div>;
+  return (
+    <div className="w-60 h-96 bg-slate-100 mb-48 rounded-lg p-2 flex flex-col gap-2">
+      <div className="w-full h-10 flex justify-center items-center p-1">
+        <input type="text" className="w-full h-full text-slate-800"></input>
+      </div>
+      <div className="w-full h-full p-1 flex justify-center items-center">
+        <div className="w-full h-full bg-slate-300 rounded"></div>
+      </div>
+    </div>
+  );
 }
