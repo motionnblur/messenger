e1aff58689edf66073059aabb0f888db8b680f95
chill <chill@mail.com>
2024-08-27 12:27:44 +0300

    Test jotai atoms

--- components/ChatCanvas.tsx
+++ components/ChatCanvas.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 import React from "react";
 import ChatLine from "./ChatLine";
+import { useAtomValue } from "jotai";
+import { textAtom } from "@/state/atoms";
 
 export default function ChatCanvas() {
+  const text = useAtomValue(textAtom);
+  console.log(text);
   return (
     <div className="w-full h-full p-1">
       <div className="w-full h-full flex flex-col bg-white rounded-sm">
--- components/SendBar.tsx
+++ components/SendBar.tsx
@@ -1,14 +1,27 @@
-import React from "react";
+"use client";
+
+import { textAtom } from "@/state/atoms";
+import { useSetAtom } from "jotai";
+import React, { useRef } from "react";
 import { Send } from "react-feather";
 
 export default function SendBar() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const setText = useSetAtom(textAtom);
+
   return (
     <div className="bottom-0 w-full h-[15%] flex flex-row items-center justify-center p-1 gap-1">
       <input
         type="text"
         className="w-full h-full bg-white rounded-lg text-slate-800"
+        ref={inputRef}
       />
-      <div className="w-10 h-full rounded-full cursor-pointer bg-slate-50 flex items-center justify-center">
+      <div
+        className="w-10 h-full rounded-full cursor-pointer bg-slate-50 flex items-center justify-center"
+        onPointerUp={() => {
+          setText(inputRef.current?.value!);
+        }}
+      >
         <Send color="black" size={30} />
       </div>
     </div>
