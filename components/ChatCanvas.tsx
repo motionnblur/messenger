a2a00624c2c6fc1828d851096c09fb35c6f3b1cb
chill <chill@mail.com>
2024-08-27 09:18:41 +0300

    Add Chat Canvas

---
+++ components/ChatCanvas.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ChatCanvas() {
+  return <div className="w-full h-full"></div>;
+}
--- components/ChatWindow.tsx
+++ components/ChatWindow.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import SendBar from "./SendBar";
+import ChatCanvas from "./ChatCanvas";
 
 export default function ChatWindow() {
   return (
     <div className="w-full h-full bg-slate-300 rounded flex flex-col">
-      <div className="w-full h-full"></div>
+      <ChatCanvas />
       <SendBar />
     </div>
   );
