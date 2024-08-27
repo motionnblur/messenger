c9783ec450714c92f3b1e2220221688836378abf
chill <chill@mail.com>
2024-08-27 11:24:10 +0300

    Update SendBar

--- components/SendBar.tsx
+++ components/SendBar.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { Send } from "react-feather";
 
 export default function SendBar() {
   return (
-    <div className="bottom-0 w-full h-1/6 flex flex-row items-center justify-center p-2 gap-1">
+    <div className="bottom-0 w-full h-[15%] flex flex-row items-center justify-center p-1 gap-1">
       <input
         type="text"
         className="w-full h-full bg-white rounded-lg text-slate-800"
       />
-      <div className="w-10 h-full bg-black rounded-full cursor-pointer"></div>
+      <div className="w-10 h-full rounded-full cursor-pointer bg-slate-50 flex items-center justify-center">
+        <Send color="black" size={30} />
+      </div>
     </div>
   );
 }
