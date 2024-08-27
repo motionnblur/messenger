cd97b20b2418959d4b4195ea234cc2d729a77eb0
chill <chill@mail.com>
2024-08-27 11:28:10 +0300

    Update Chat Canvas

--- components/ChatCanvas.tsx
+++ components/ChatCanvas.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function ChatCanvas() {
-  return <div className="w-full h-full"></div>;
+  return (
+    <div className="w-full h-full p-1">
+      <div className="w-full h-full flex flex-col bg-white rounded-sm"></div>
+    </div>
+  );
 }
