ff39a555aaf788e74e065e0d736b5bea75c31e64
chill <chill@mail.com>
2024-08-31 02:02:02 +0300

    Add login page template

--- server/rest-server/src/main/java/com/server/rest_server/controller/SignUpController.java
+++ server/rest-server/src/main/java/com/server/rest_server/controller/SignUpController.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("signup")
 public class SignUpController {
     @Autowired
     protected UserRepository userRepository;
