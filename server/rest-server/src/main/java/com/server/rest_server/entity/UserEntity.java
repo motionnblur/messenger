adbef3e282926d415e6785f2814c07133e31ce25
chill <chill@mail.com>
2024-08-31 01:15:19 +0300

    Fix postgres container
    Prevent the container from being exposed to public.
    Fix UserEntity Id.

--- server/rest-server/src/main/java/com/server/rest_server/entity/UserEntity.java
+++ server/rest-server/src/main/java/com/server/rest_server/entity/UserEntity.java
@@ -5,7 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import lombok.*;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.Id;
 
 @Entity
 @Data
