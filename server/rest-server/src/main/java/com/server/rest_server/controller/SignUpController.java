2356d2c85bddffe3cd941a60e6a63d1a692004df
chill <chill@mail.com>
2024-08-30 22:34:25 +0300

    Add SignUpController

---
+++ server/rest-server/src/main/java/com/server/rest_server/controller/SignUpController.java
@@ -0,0 +1,9 @@
+package com.server.rest_server.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("signup")
+public class SignUpController {
+}
