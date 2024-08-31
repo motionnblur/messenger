59b2ad4809861cec0264b942f7214ddc907ab2a3
chill <chill@mail.com>
2024-08-31 22:28:09 +0300

    Update on messenger ui
    Add shadow

--- client/components/ChatLine.tsx
+++ client/components/ChatLine.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function ChatLine({ text }: { text: IMessage }) {
   return (
     <div className="w-full h-11 flex justify-center items-center p-1">
-      <div className="w-full h-full bg-slate-200 rounded-lg p-1">
+      <div className="w-full h-full bg-slate-300 rounded-lg p-1">
         <div className="w-full h-full text-slate-900 text-lg">
           {text.userName + ": " + text.message}
         </div>
