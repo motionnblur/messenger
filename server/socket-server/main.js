2767a11b5aa4e9d5a9a05a3bd82cda430e736616
chill <chill@mail.com>
2024-09-02 19:07:15 +0300

    Add time expire to sockets

--- server/socket-server/main.js
+++ server/socket-server/main.js
@@ -1,13 +1,36 @@
 const { Server } = require("socket.io");
+const { performance } = require("perf_hooks");
+
 const io = new Server({
   cors: {
     origin: "*",
   },
 });
 
+var list = [];
+
 io.on("connect", (socket) => {
+  list.push({
+    key: socket.id,
+    time: performance.now(),
+  });
+
   socket.on("message", (messageJson) => {
-    io.emit("broadcast", messageJson);
+    list.forEach((json) => {
+      if (json.key == socket.id) {
+        const lastTime = json.time;
+
+        const currentTime = performance.now();
+        const deltaTime = (currentTime - lastTime) / 1000;
+
+        if (deltaTime > 60 * 2) {
+          socket.disconnect();
+          return;
+        }
+
+        io.emit("broadcast", messageJson);
+      }
+    });
   });
 });
 
