dc37ae750daba018539ea44f8c263eecac636ad9
chill <chill@mail.com>
2024-09-12 19:11:14 +0300

    Improve SendBar
    Send message by pressing Enter key

--- client/components/SendBar.tsx
+++ client/components/SendBar.tsx
@@ -2,13 +2,42 @@
 
 import { useAtomValue } from "jotai";
 import { socket } from "../socket";
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { Send } from "react-feather";
 import { userName } from "@/state/atoms";
 
 export default function SendBar() {
   const inputRef = useRef<HTMLInputElement>(null);
   const useUserName = useAtomValue(userName);
+  const sendMessage = () => {
+    var text: string = inputRef.current?.value!;
+    if (text === null || !text) return;
+    const id = localStorage.getItem("sessionId");
+    if (id === null && id === undefined) return;
+
+    if (text.length > 100) {
+      alert("Message length should be equal or less than 100");
+      return;
+    }
+
+    socket.emit("message", {
+      sessionId: id,
+      userName: useUserName,
+      message: text,
+    });
+    inputRef.current!.value! = "";
+  };
+
+  useEffect(() => {
+    function onKeyDown(e: KeyboardEvent) {
+      if (e.key !== "Enter") return;
+      sendMessage();
+    }
+    document.addEventListener("keydown", onKeyDown);
+    return () => {
+      document.removeEventListener("keydown", onKeyDown);
+    };
+  }, []);
 
   return (
     <div className="bottom-0 w-full h-14 flex flex-row items-center justify-center p-2 gap-1">
@@ -22,22 +51,7 @@ export default function SendBar() {
       <div
         className="w-10 h-full rounded-full cursor-pointer bg-slate-50 flex items-center justify-center"
         onPointerUp={() => {
-          var text: string = inputRef.current?.value!;
-          if (text === null || !text) return;
-          const id = localStorage.getItem("sessionId");
-          if (id === null && id === undefined) return;
-
-          if (text.length > 100) {
-            alert("Message length should be equal or less than 100");
-            return;
-          }
-
-          socket.emit("message", {
-            sessionId: id,
-            userName: useUserName,
-            message: text,
-          });
-          inputRef.current!.value! = "";
+          sendMessage();
         }}
       >
         <Send color="black" size={30} />
