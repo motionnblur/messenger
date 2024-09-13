c98cc1909a35669713c98047813e9ba60b35614e
chill <chill@mail.com>
2024-09-13 20:58:10 +0300

    Refactor

--- server/rest-server/src/main/java/com/server/rest_server/controller/MessengerController.java
+++ server/rest-server/src/main/java/com/server/rest_server/controller/MessengerController.java
@@ -1,9 +1,5 @@
 package com.server.rest_server.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.server.rest_server.dto.SessionEntityDto;
-import com.server.rest_server.entity.SessionEntity;
-import com.server.rest_server.repository.SessionRepository;
 import com.server.rest_server.service.SessionEntityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,7 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.DataInput;
 import java.util.Map;
 
 @RestController
@@ -23,17 +18,7 @@ public class MessengerController {
     @PostMapping("/saveMessages")
     private ResponseEntity<?> saveMessages(@RequestBody Map<String, Object> payload) {
         try{
-            SessionEntityDto sDto = new SessionEntityDto();
-
-            String sessionId = (String) payload.get("sessionId");
-            String userName = (String) payload.get("userName");
-            String[] messages = (String[]) payload.get("messages");
-
-            sDto.setSessionId(sessionId);
-            sDto.setUserName(userName);
-            sDto.setMessages(messages);
-
-            sessionEntityService.saveSession(sDto);
+            sessionEntityService.saveSession(payload);
 
             return new ResponseEntity<>(HttpStatus.OK);
         }catch (Exception e){
--- server/rest-server/src/main/java/com/server/rest_server/service/SessionEntityService.java
+++ server/rest-server/src/main/java/com/server/rest_server/service/SessionEntityService.java
@@ -1,21 +1,31 @@
 package com.server.rest_server.service;
 
-import com.server.rest_server.dto.SessionEntityDto;
 import com.server.rest_server.entity.SessionEntity;
 import com.server.rest_server.repository.SessionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 public class SessionEntityService {
     @Autowired
     protected SessionRepository sessionRepository;
-    public void saveSession(SessionEntityDto sDto) {
-        SessionEntity se = new SessionEntity();
-        se.setSessionId(sDto.getSessionId());
-        se.setUserName(sDto.getUserName());
-        se.setMessages(sDto.getMessages());
 
-        sessionRepository.save(se);
+    public void saveSession(Map<String, Object> payload) throws Exception {
+        SessionEntity sDto = new SessionEntity();
+
+        String sessionId = (String) payload.get("sessionId");
+        String userName = (String) payload.get("userName");
+        String[] messages = (String[]) payload.get("messages");
+
+        if(sessionId == null || userName == null || messages == null)
+            throw new Exception("Session save error");
+
+        sDto.setSessionId(sessionId);
+        sDto.setUserName(userName);
+        sDto.setMessages(messages);
+
+        sessionRepository.save(sDto);
     }
 }
