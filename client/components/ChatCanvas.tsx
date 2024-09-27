faf32ec05cf73edcd3566b72468ca88a844cc4c9
chill <chill@mail.com>
2024-09-27 09:03:59 +0300

    Update 2

--- client/components/ChatCanvas.tsx
+++ client/components/ChatCanvas.tsx
@@ -5,11 +5,32 @@ import ChatLine from "./ChatLine";
 import { useAtomValue } from "jotai";
 import { broadCastJson, sessionMessage } from "@/state/atoms";
 
+var lastMessageArr: IMessage[] = [];
 var messageArr: IMessage[] = [];
+var factor: number = 1;
+
+function arraysEqual<T>(arr1: T[], arr2: T[]): boolean {
+  // Check if lengths are different
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  // Check each element
+  for (let i = 0; i < arr1.length; i++) {
+    // Use JSON.stringify for deep comparison of objects
+    if (JSON.stringify(arr1[i]) !== JSON.stringify(arr2[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 export default function ChatCanvas() {
   const useSessionMessage = useAtomValue(sessionMessage);
   const useBroadCastJson = useAtomValue(broadCastJson);
+
+  const [render, setRender] = React.useState(false);
 
   const canvasRef = React.useRef<HTMLDivElement>(null);
   const uploadRef = React.useRef<HTMLDivElement>(null);
@@ -53,18 +74,28 @@ export default function ChatCanvas() {
         bg-teal-300 w-14 h-14 rounded-full cursor-pointer"
           ref={uploadRef}
           onClick={() => {
-            fetch(process.env.NEXT_PUBLIC_PREVIOUS_MESSAGE! + "?factor=" + 1, {
-              method: "GET",
-              headers: {
-                "Content-Type": "application/json",
-              },
-            })
+            fetch(
+              process.env.NEXT_PUBLIC_PREVIOUS_MESSAGE! + "?factor=" + factor,
+              {
+                method: "GET",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              }
+            )
               .then((res) => {
                 if (!res.ok) throw new Error("Error");
                 return res.json();
               })
-              .then((d: any) => {
-                console.log(d);
+              .then((data: IMessage[]) => {
+                const temp: IMessage[] = data;
+                if (arraysEqual(temp, lastMessageArr)) return;
+
+                messageArr.unshift(...data);
+
+                lastMessageArr = data;
+                factor += 1;
+                setRender(!render);
               });
           }}
         />
