3225429e7256370e3012c0bab6f00df8f0006b97
chill <carbonfiberdev@gmail.com>
2024-09-29 07:30:59 +0300

    Add Arrow icon

--- client/components/SessionLoader.tsx
+++ client/components/SessionLoader.tsx
@@ -1,6 +1,7 @@
 import { arraysEqual } from "@/logic/fPublic";
 import { memory } from "@/logic/memory";
 import React from "react";
+import { ArrowUp } from "react-feather";
 
 export default function SessionLoader({
   uploadRef,
@@ -14,38 +15,36 @@ export default function SessionLoader({
   render: boolean;
 }) {
   return (
-    <div
-      className="absolute left-1/2 -translate-x-1/2 top-5 transform
-  bg-teal-300 w-14 h-14 rounded-full cursor-pointer"
-      ref={uploadRef}
-      onClick={() => {
-        fetch(
-          process.env.NEXT_PUBLIC_PREVIOUS_MESSAGE! +
-            "?factor=" +
-            memory.factor,
-          {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        )
-          .then((res) => {
-            if (!res.ok) throw new Error("Error");
-            return res.json();
-          })
-          .then((data: IMessage[]) => {
-            const temp: IMessage[] = data;
-            if (arraysEqual(temp, memory.lastMessageArr)) return;
+    <ArrowUp size={30} className="absolute left-1/2 -translate-x-1/2 top-5 transform
+  bg-slate-900 w-14 h-14 rounded-full cursor-pointer"  ref={uploadRef}
+  onClick={() => {
+    fetch(
+      process.env.NEXT_PUBLIC_PREVIOUS_MESSAGE! +
+        "?factor=" +
+        memory.factor,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => {
+        if (!res.ok) throw new Error("Error");
+        return res.json();
+      })
+      .then((data: IMessage[]) => {
+        const temp: IMessage[] = data;
+        if (arraysEqual(temp, memory.lastMessageArr)) return;
 
-            messageArr.unshift(...data);
+        messageArr.unshift(...data);
 
-            memory.lastMessageArr = data;
-            memory.factor += 1;
-            setRender(!render);
-          })
-          .catch((err) => console.log(err));
-      }}
-    />
+        memory.lastMessageArr = data;
+        memory.factor += 1;
+        setRender(!render);
+      })
+      .catch((err) => console.log(err));
+  }}/>
+
   );
 }
